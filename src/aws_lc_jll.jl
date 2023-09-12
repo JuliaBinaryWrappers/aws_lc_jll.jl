b134d6f545fe6b9eefd468b2d77c7315f45b42d4
jlbuild <juliabuildbot@gmail.com>
2023-09-12 20:51:34 +0000

    aws_lc_jll build 1.15.0+0

---
+++ src/aws_lc_jll.jl
@@ -0,0 +1,9 @@
+# Use baremodule to shave off a few KB from the serialized `.ji` file
+baremodule aws_lc_jll
+using Base
+using Base: UUID
+import JLLWrappers
+
+JLLWrappers.@generate_main_file_header("aws_lc")
+JLLWrappers.@generate_main_file("aws_lc", UUID("97bc56f7-b146-5eb0-9ba6-f74d48471a93"))
+end  # module aws_lc_jll
